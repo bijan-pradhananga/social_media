d07763b7281317841902599969f6960cc6dd47b6
Bijan <bijan2059@gmail.com>
2024-02-21 07:55:22 +0545

    added elements to login page

--- insta_frontend/src/components/login_page/Login.jsx
+++ insta_frontend/src/components/login_page/Login.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <h2>Login</h2>
+      <input type="text" placeholder='Enter your email' required/> <br />
+      <input type="password" placeholder='Enter your password' required/> <br />
+      <button style={{color:'black'}}>Login</button>
+    </div>
   )
 }
