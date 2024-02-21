7ad3f5bae004d5f085cb760112885659beda0202
Bijan <bijan2059@gmail.com>
2024-02-21 07:56:01 +0545

    added elements to register page

--- insta_frontend/src/components/register_page/Register.jsx
+++ insta_frontend/src/components/register_page/Register.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div>
+      <h2>Register</h2>
+        <form action="" method="post">
+        <label htmlFor="name">Name</label><br />
+        <input type="text" id='name' /><br />
+        <label htmlFor="username">Username</label><br />
+        <input type="text" id='username' /><br />
+        <label htmlFor="emai;">Email</label><br />
+        <input type="email" id='emai;' /><br />
+        <label htmlFor="password">Password</label><br />
+        <input type="password" id='password' /><br />
+        <label htmlFor="image">Image</label><br />
+        <input type="file" id='image' /><br />
+        <button style={{color:'black'}}>Register</button>
+      </form>
+
+    </div>
   )
 }
