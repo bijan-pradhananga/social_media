fc64c65acd8d699034b4e9d7b17b1ffd8c6c4ed9
Bijan <bijan2059@gmail.com>
2024-03-13 17:24:31 +0545

    made register api

---
+++ insta_backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'  => 'required|max:100',
+            'username'   => 'required|max:100',
+            'email' => 'required|email|max:100',
+            'password' => 'required|string|min:8',
+            'image'    => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => $validator->messages()
+            ], 422);
+        } else {
+            $image = $request->file('image');
+            $imgName = $image->getClientOriginalName();
+            $user = User::create([
+                'name'  => $request->name,
+                'username'   => $request->username,
+                'email' => $request->email,
+                'password'        => Hash::make($request->password),
+                'image'         => $imgName
+            ]);
+            $image->move('images/',$imgName);
+        }
+        if ($user) {
+            return response()->json([
+                'status' => 200,
+                'message' => 'User added successfully'
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Something went wrong'
+            ], 500);
+        }
+    }
+}
--- insta_frontend/src/components/register_page/Register.jsx
+++ insta_frontend/src/components/register_page/Register.jsx
@@ -1,23 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function Register() {
+    const [formData, setFormData] = useState({
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+      image: ''
+  });
+
+  //to change the value of the form inputs
+  const handleChange = (e) => {
+      setFormData({
+          ...formData,
+          [e.target.id]: e.target.value
+      });
+  };
+
+  //to handle image change
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setFormData({
+        ...formData,
+        image:file
+    });
+  };
+
+  const addData = async  (formData) =>{
+    try {
+      let fn = new FormData();
+      Object.keys(formData).forEach(key => {
+        fn.append(key,formData[key]);
+      });
+      const response = await axios.post('http://127.0.0.1:8000/api/users',fn);
+      if (response.status === 200) {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      //adding the data on submit
+      addData(formData);
+      // clearing the inputs in the form
+      setFormData({
+        name: '',
+        username: '',
+        email: '',
+        password: '',
+        image: ''
+      });
+  }
   return (
     <div>
       <h2>Register</h2>
-        <form action="" method="post">
+      <form id='registerForm' method="post" onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label><br />
-        <input type="text" id='name' /><br />
+        <input type="text" id='name' name='name' value={formData.name} onChange={handleChange} required/><br />
         <label htmlFor="username">Username</label><br />
-        <input type="text" id='username' /><br />
+        <input type="text" id='username' name='username' value={formData.username} onChange={handleChange} required/><br />
         <label htmlFor="emai;">Email</label><br />
-        <input type="email" id='emai;' /><br />
+        <input type="email" id='email' name='email' value={formData.email} onChange={handleChange} required/><br />
         <label htmlFor="password">Password</label><br />
-        <input type="password" id='password' /><br />
+        <input type="password" id='password' name='password' value={formData.password} onChange={handleChange} required/><br />
         <label htmlFor="image">Image</label><br />
-        <input type="file" id='image' /><br />
+        <input type="file" id='image' name='image' onChange={handleImageChange} required/><br />
         <button style={{color:'black'}}>Register</button>
       </form>
-
     </div>
   )
 }
