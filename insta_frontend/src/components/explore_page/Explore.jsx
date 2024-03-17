c812c77bbc8fc226d417ef31202913edf351a296
Bijan <bijan2059@gmail.com>
2024-03-17 18:43:26 +0545

    divided explore page

--- insta_frontend/src/components/explore_page/Explore.jsx
+++ insta_frontend/src/components/explore_page/Explore.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import './explore.css'
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12]
+import ExploreBody from './ExploreBody'
 export default function Explore() {
   return (
     <div className='explore_contents'>
       <div className='explore-header'>
           <h2>Explore</h2>
       </div>
-      <div className="explore-body">
-        {array.map((img,index)=>
-          <div className='explore-body-img' key={index}>
-
-          </div>
-        )}
-      </div>
+      <ExploreBody/>
     </div>
   )
 }
