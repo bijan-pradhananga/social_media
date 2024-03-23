a4c9d206c9e123618a88612858a069ff1d6fbe2d
Bijan <bijan2059@gmail.com>
2024-03-23 09:44:55 +0545

    removed the setImgPopupLikes function

--- insta_frontend/src/components/home_page/Timeline/TimelineCard/TimelineCardImg.jsx
+++ insta_frontend/src/components/home_page/Timeline/TimelineCard/TimelineCardImg.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function TimelineCardImg({post,setImgPopup,setImgPopupDetails,setImgPopupLikes,likeCount}) {
+export default function TimelineCardImg({post,setImgPopup,setImgPopupDetails,likeCount}) {
   return (
-    <div className="timeline_pic" style={{cursor:'pointer'}} onClick={() => { setImgPopup(true); setImgPopupDetails(post); setImgPopupLikes(likeCount)  }}>
+    <div className="timeline_pic" style={{cursor:'pointer'}} onClick={() => { setImgPopup(true); setImgPopupDetails(post);  }}>
       <img src={`http://127.0.0.1:8000/posts/${post.image}`} alt="" />
     </div>
   )
