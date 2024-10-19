94e94f57e87de3290c7fcd34ea9e3e86b6c77bd5
Bijan Pradhananga <100182448+bijan-pradhananga@users.noreply.github.com>
2024-10-19 18:01:19 +0545

    Update config.js

--- insta_frontend/src/api/config.js
+++ insta_frontend/src/api/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL:'http://127.0.0.1:8000',
+    baseURL:'https://socialmedia-production-f7a5.up.railway.app',
     withCredentials:true
 })
